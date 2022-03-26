207d41bec3a5149f300735002bfd41c16eee8439
gaston pedraza <gastonpedraza.developer@gmail.com>
2022-03-26 20:33:10 -0300

    remove unnecesary import

--- client/src/pages/results/components/Pagination/Pagination.jsx
+++ client/src/pages/results/components/Pagination/Pagination.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import queryString from 'query-string';
