ac2a51c932750e25705b0869b056947a07952132
gaston pedraza <gp.ju.dev@gmail.com>
2022-05-22 12:38:47 -0300

    add tooltip to button

--- client/src/components/SlideProducts/Product/Product.jsx
+++ client/src/components/SlideProducts/Product/Product.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Box from '@material-ui/core/Box';
 import CardContent from '@material-ui/core/CardContent';
+import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
@@ -101,11 +102,13 @@ export default function Product(props) {
 				<Typography className={classes.price} display="block" gutterBottom>
 					$ {product.price}
 				</Typography>
-				<IconButton 
-					onClick={(e) => addToCar(e, product._id)}
-				>
-					<ShoppingCartIcon />
-				</IconButton>
+				<Tooltip title="Añadir al carrito" placement="bottom">
+					<IconButton 
+						onClick={(e) => addToCar(e, product._id)}
+					>
+						<ShoppingCartIcon />
+					</IconButton>
+				</Tooltip>
 			</div>
 		</Card>
 	);
